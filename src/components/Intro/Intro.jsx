e1af9c6d01cddfa7320504ff7b1af1db936b303f
SherlockeDBOY <aswintherealone@gmail.com>
2021-06-15 14:46:32 +0530

    first commit

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -16,8 +16,8 @@ const Intro =  ({data}) => {
                             <div className="desc">
                                 <p>{data.desc}</p>
                             </div>
-                            <Link to={process.env.PUBLIC_URL + "/"} className="btn btn-primary btn-hover-secondary">Explore More</Link>
-                            <Link to={process.env.PUBLIC_URL + "/"} className="btn btn-outline-white btn-hover-primary"> All Services </Link>
+                            <Link to={process.env.PUBLIC_URL + "/about"} className="btn btn-primary btn-hover-secondary">Explore More</Link>
+                            <Link to={process.env.PUBLIC_URL + "/service"} className="btn btn-outline-white btn-hover-primary"> All Services </Link>
                         </div>
                     </div>
 
